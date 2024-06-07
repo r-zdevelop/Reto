85d2c21fa687c18c3545cb33da5e4578ed1ca89e
Regynald Zambrano <regynald.zambrano@gmail.com>
2024-06-07 08:26:48 -0500

    header done

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Header = () => {
+    return (
+        <View style={{
+            width: "100%",
+            height: 90,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderBottomColor: '#eef1f6',
+            borderBottomWidth: 2,
+        }}>
+            <Text style={{
+                color: '#0f265c',
+                textTransform: 'uppercase',
+                fontWeight: 'bold',
+                fontSize: 20,
+            }}>Banco</Text>
+        </View>
+    );
+}
+
+export default Header;
